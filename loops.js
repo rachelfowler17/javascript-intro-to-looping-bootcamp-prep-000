0935f5b50709ecb5ee5d759385d5e027caff2abf
Rachel Fowler <rachelfowler17@gmail.com>
2019-04-28 17:53:36 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,5 @@
+var ourArray = [];
+for (let i = 0; i < 25; i++) {console.log ("I am ${i} strange loops."); 
+  }  
+  
+  function whileLoop() { return Math.random() >= 1; while (maybeTrue()) {console.log("DONE!"); } 
